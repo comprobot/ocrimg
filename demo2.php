7f3597c85e3afb64dc6f7485efbf909a98a1aa10
comprobot <comprobot0503@gmail.com>
2018-06-07 17:11:46 +0800

    Update demo2.php

--- demo2.php
+++ demo2.php
@@ -1,4 +1,45 @@
 <?php
+function http($url, $params = array(), $method = 'GET', $ssl = false){
+    $opts = array(
+        CURLOPT_TIMEOUT        => 30,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => false
+    );
+    /* 根据请求类型设置特定参数 */
+    switch(strtoupper($method)){
+        case 'GET':
+            $getQuerys = !empty($params) ? '?'. http_build_query($params) : '';
+            $opts[CURLOPT_URL] = $url . $getQuerys;
+            break;
+        case 'POST':
+            $opts[CURLOPT_URL] = $url;
+            $opts[CURLOPT_POST] = 1;
+            $opts[CURLOPT_POSTFIELDS] = $params;
+            break;
+    }
+    if ($ssl) {
+        $opts[CURLOPT_SSLCERTTYPE] = 'PEM';
+        $opts[CURLOPT_SSLCERT]     = $ssl['cert'];
+        $opts[CURLOPT_SSLKEYTYPE]  = 'PEM';
+        $opts[CURLOPT_SSLKEY]      = $ssl['key'];;
+    }
+    /* 初始化并执行curl请求 */
+    $ch     = curl_init();
+    curl_setopt_array($ch, $opts);
+    $data   = curl_exec($ch);
+    $err    = curl_errno($ch);
+    $errmsg = curl_error($ch);
+    curl_close($ch);
+    if ($err > 0) {
+        $this->error = $errmsg;
+        return false;
+    }else {
+        return $data;
+    }
+}
+?>
+<?php
 
 require('./HttpUtil.php');
 
@@ -23,8 +64,12 @@ $params = array(
 
 // 执行API调用
 $url = 'https://api.ai.qq.com/fcgi-bin/ocr/ocr_generalocr';
-$response = doHttpPost($url, $params);
-echo $response;
+$data = http($url, $params,"POST");
+$data=json_decode($data,true)['data']['item_list'];
+    $res='';
+    foreach ($data as $k => $v) {
+        $res.=$v['itemstring']."<br>";
+    }
+    echo $res;
 
-
-
+?>
