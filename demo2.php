5964ceba60633c2e7f03cc9ade6bf2187fd9e363
comprobot <comprobot0503@gmail.com>
2018-06-07 16:46:46 +0800

    Update demo2.php

--- demo2.php
+++ demo2.php
@@ -1,20 +1,30 @@
 <?php
 
-require('./include.php');
 
-//请在此填入AppID与AppKey
-$app_id = '1106872796';
-$app_key = 'V27D1dj9Pz3uA3E7';
 
-//设置AppID与AppKey
-Configer::setAppInfo($app_id, $app_key);
-
-// 通用OCR识别
 $image_data = file_get_contents('./data/generalocr.jpg');
+
 $params = array(
     'image' => base64_encode($image_data),
 );
-$response = API::generalocr($params);
-var_dump($response);
+
+echo "testing.."
+$base64 = base64_encode($image_data);
+
+// 设置请求数据
+$appkey = 'V27D1dj9Pz3uA3E7';
+$params = array(
+    'app_id'     => '1106872796',
+    'image'      => $base64,
+    'time_stamp' => strval(time()),
+    'nonce_str'  => strval(rand()),
+    'sign'       => '',
+);
+
+// 执行API调用
+$url = 'https://api.ai.qq.com/fcgi-bin/ocr/ocr_generalocr';
+$response = doHttpPost($url, $params);
+echo $response;
+
 
 
