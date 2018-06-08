a79fa150bf83b6639af41942bd6b7df92ea1e4c4
comprobot <comprobot0503@gmail.com>
2018-06-08 12:18:01 +0800

    Update crawl.php

--- crawl.php
+++ crawl.php
@@ -97,6 +97,9 @@
                 echo 'sign  : <br/>';
 		echo $params['sign'];
                 echo '<br/>'	;		
+		echo 'image b4 base4  : <br/>';
+		echo $image_data;	
+		echo '<br/>'	;	                
                 echo 'image  : <br/>';
 		echo $params['image'];	
 		echo '<br/>'	;	
